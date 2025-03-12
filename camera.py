98932a90f74f3df99ad69e53f94900d3c306e9de
Tim <tim.andrejc02@gmail.com>
2025-03-12 16:19:21 +0100

    Changed detection from average color to pixel by pixel

--- camera.py
+++ camera.py
@@ -13,13 +13,30 @@ def obdelaj_sliko_s_skatlami(slika, sirina_skatle, visina_skatle, barva_koze) ->
     Primer: Če je v sliki 25 škatel, kjer je v vsaki vrstici 5 škatel, naj bo seznam oblike
       [[1,0,0,1,1],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[1,0,0,0,1]].
       V tem primeru je v prvi škatli 1 piksel kože, v drugi 0, v tretji 0, v četrti 1 in v peti 1.'''
+    for i in range(0, camy, visina_skatle):
+        for j in range(0, camx, sirina_skatle):
+            tempx = sirina_skatle
+            tempy = visina_skatle
+            if (j + x > camx):
+                tempx = camx - j
+            if (i + y > camy):
+                tempy = camy - i
+            if prestej_piklse_z_barvo_koze(slika[i:i + tempy, j:j + tempx], barva_koze) > sirina_skatle * visina_skatle/ 2:
+                cv.rectangle(frame, (j, i), (j + tempx, i + tempy), (0, 255, 0), 3)
+            else:
+                cv.rectangle(frame, (j, i), (j + tempx, i + tempy), (0, 0, 255), 1)
     pass
 
 
 def prestej_piklse_z_barvo_koze(slika, barva_koze) -> int:
     '''Prestej število pikslov z barvo kože v škatli.'''
-    pass
-
+    res = 0
+    for i in range(slika.shape[0]):
+        for j in range(slika.shape[1]):
+            if (barva_koze[0] < slika[i][j][0] < barva_koze[3] and barva_koze[1] < slika[i][j][1] < barva_koze[4]
+                and barva_koze[2] < slika[i][j][2] < barva_koze[5]):
+                    res += 1
+    return res
 
 def doloci_barvo_koze(slika, levo_zgoraj, desno_spodaj) -> tuple:
     '''Ta funkcija se kliče zgolj 1x na prvi sliki iz kamere.
@@ -34,7 +51,8 @@ def doloci_barvo_koze(slika, levo_zgoraj, desno_spodaj) -> tuple:
             g += int(slika[i, j][0]) / count
             b += int(slika[i, j][1]) / count
             r += int(slika[i, j][2]) / count
-    return tuple([int(g), int(b), int(r)])
+    return tuple([int(g) - tolerance, int(b) - tolerance, int(r) - tolerance, int(g) + tolerance,
+                 int(b) + tolerance, int(r) + tolerance])
 
 def povprecna_barva(slika, sirina, visina) -> tuple:
     g = 0
@@ -49,15 +67,33 @@ def povprecna_barva(slika, sirina, visina) -> tuple:
             r += int(slika[i, j][2]) / div
 
     return tuple([int(g), int(b), int(r)])
+
+def staro_barvanje():
+    for i in range(0, camy, y):
+        for j in range(0, camx, x):
+            tempx = x
+            tempy = y
+            if (j + x > camx):
+                tempx = camx - j
+            if (i + y > camy):
+                tempy = camy - i
+            barva = povprecna_barva(frame[i:i + y, j:j + x], tempx, tempy)
+            if (skin[0] + tolerance > barva[0] > skin[0] - tolerance and
+                    skin[1] + tolerance > barva[1] > skin[1] - tolerance and
+                    skin[2] + tolerance > barva[2] > skin[2] - tolerance):
+                cv.rectangle(frame, (j, i), (j + x, i + y), (0, 255, 0), 2)
+            else:
+                cv.rectangle(frame, (j, i), (j + x, i + y), (0, 0, 255), 1)
+
 if __name__ == '__main__':
     # Pripravi kamero
-    tolerance = 30
     camy = 220
     camx = 340
-    x = 30
-    y = 30
+    x = 20
+    y = 20
+    tolerance = 50
     skin = 0
-    camera = cv.VideoCapture(1)
+    camera = cv.VideoCapture(0)
     ret, frame = camera.read()
     num = 0
     while True:
@@ -66,28 +102,14 @@ if __name__ == '__main__':
             frame = cv.flip(frame, 1)
             frame = zmanjsaj_sliko(frame, camx, camy)
             if num < 40:
-                cv.rectangle(frame, [140, 50], [210,150], (255, 0, 0), 3)
+                frame = cv.rectangle(frame, [140, 50], [210,150], (255, 0, 0), 3)
                 num += 1
             elif num == 40:
                 skin = doloci_barvo_koze(frame, [140, 50], [210, 150])
-                #print(skin)
+                print(skin)
                 num += 1
             else:
-                for i in range(0, camy, y):
-                    for j in range(0, camx, x):
-                        tempx = x
-                        tempy = y
-                        if(j + x > camx):
-                            tempx = camx - j
-                        if(i + y > camy):
-                            tempy = camy - i
-                        barva = povprecna_barva(frame[i:i + y, j:j + x], tempx, tempy)
-                        if (skin[0] + tolerance > barva[0] > skin[0] - tolerance and
-                                skin[1] + tolerance > barva[1] > skin[1] - tolerance and
-                                skin[2] + tolerance > barva[2] > skin[2] - tolerance):
-                            cv.rectangle(frame, (j, i), (j + x, i + y), (0, 255, 0), 2)
-                        else:
-                            cv.rectangle(frame, (j, i), (j + x, i + y), (0, 0, 255), 1)
+                obdelaj_sliko_s_skatlami(frame, x, y, skin)
             cv.imshow('frame', frame)
         if(cv.waitKey(1) & 0xFF == ord('q')):
             break
